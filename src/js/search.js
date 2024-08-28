39eb3487e3a5cafa4dc0f39544c8f29b9cfee637
sandrarolgom <sandraroldan210900@gmail.com>
2024-08-28 14:05:29 +0200

    crear evento click sobre cada elemento del array para añadirle la clase favorites

--- src/js/search.js
+++ src/js/search.js
@@ -3,12 +3,13 @@ const searchBtn = document.querySelector('.js-searchBtn');
 const sectionAnimes = document.querySelector('.js-section2')
 
 let arrayAnime = []; //array general que en el que vamos a meter los datos para crear otros arrays más adelante ej. array de datos de naruto/array filtrado
+let animesFavorites = [] //array que hay que llenar con los animes clicados
 
 const renderAnimes = (animes)=>{
+    sectionAnimes.innerHTML='';
     for (const eachAnime of animes){
-        sectionAnimes.innerHTML='';
-
         const listAnimes = document.createElement('article');
+        listAnimes.setAttribute('id', eachAnime.mal_id)
         sectionAnimes.appendChild(listAnimes);
     
         const title = document.createElement('h2');
@@ -20,6 +21,22 @@ const renderAnimes = (animes)=>{
         image.setAttribute('src', eachAnime.images.jpg.image_url)
         image.setAttribute('alt', titleName);
         listAnimes.appendChild(image);
+
+        const handleClickFav = (event)=>{
+            //listAnimes.classList.add('favorite'); esto no vale porque si quisiera guardarlo no se guardaría, es únicamente visual
+            //listAnimes.classList.toggle('favorite'); en este caso también se desmarcaría pero no lo queremos en este ejercicio
+            console.log(event.currentTarget.id);
+            const id = event.currentTarget.id;
+            //queremos buscar el id a ver sie stá en el array de favorites con findIndex (-1, si no está)
+            const indexAnimeFavorite = animesFavorites.findIndex(eachAnime=>eachAnime.id === id);
+            console.log(indexAnimeFavorite); //aquí da -1 porque aun no hay nada en el array
+            if (indexAnimeFavorite === -1){
+                //obtener toda la información del elemento clicado
+                animesFavorites.push();
+            }
+            // renderAnimes(animesFavorites);
+        }
+        listAnimes.addEventListener('click', handleClickFav)
     }
 }
 const getdataApi = (value)=>{
