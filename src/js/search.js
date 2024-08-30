62708565670f466a0eb85f8db7a369b690111fd0
sandrarolgom <sandraroldan210900@gmail.com>
2024-08-30 09:29:28 +0200

    quitar comentarios

--- src/js/search.js
+++ src/js/search.js
@@ -3,25 +3,25 @@ const searchBtn = document.querySelector('.js-searchBtn');
 const sectionAnimes = document.querySelector('.js-section2');
 const sectionAnimesFavorites = document.querySelector('.js-section1');
 
-let arrayAnime = []; //array general que en el que vamos a meter los datos para crear otros arrays más adelante ej. array de datos de naruto/array filtrado
-let animesFavorites = [] //array que hay que llenar con los animes clicados
+let arrayAnime = [];
+let animesFavorites = [];
 
 const handleClickFav = (event)=>{
-    const id = parseInt(event.currentTarget.id); //ADD PARSEINT
+    const id = parseInt(event.currentTarget.id);
 
-    const indexAnimeFavorite = animesFavorites.findIndex((item)=>item.mal_id === id); //constante que busque si el id pintado en caso de que lo haya, sea igual al id clicado, si no está, me devuelve -1
-    console.log(indexAnimeFavorite); //aquí da -1 porque aun no hay nada en el array
+    const indexAnimeFavorite = animesFavorites.findIndex((item)=>item.mal_id === id);
+    console.log(indexAnimeFavorite);
 
     if (indexAnimeFavorite === -1){
-        const animeClicked = arrayAnime.find((item)=>item.mal_id===id); //del array original, búscame el elemento cuyo id sea igual al id clicado
-        animesFavorites.push(animeClicked) //en el array de animesfavorites, mete (push) el objeto clicado que está recogido en animeClicked
+        const animeClicked = arrayAnime.find((item)=>item.mal_id===id);
+        animesFavorites.push(animeClicked);
         console.log(animesFavorites);
-        renderAnimes(arrayAnime); //tiene que ser el array general porque si ponemos el nuevo array te pinta solo el clicado
-        const saveAnimesFavorites = localStorage.setItem('animesFavorites', JSON.stringify(animesFavorites)); // aquí estoy guardando los datos clicados en el el localstorage y creando un nuevo array, le ponemos stringify porque necesitamos que entren los datos como array
+        renderAnimes(arrayAnime);
+        const saveAnimesFavorites = localStorage.setItem('animesFavorites', JSON.stringify(animesFavorites));
         renderAnimesFavorites(animesFavorites);
     }
-    
 }
+
 const renderAnimes = (animes)=>{
     sectionAnimes.innerHTML='';
     const titleSection = document.createElement('h3');
@@ -30,9 +30,9 @@ const renderAnimes = (animes)=>{
     sectionAnimes.appendChild(titleSection);
     for (const eachAnime of animes){
         const listAnimes = document.createElement('article');
-        listAnimes.setAttribute('id', eachAnime.mal_id) 
+        listAnimes.setAttribute('id', eachAnime.mal_id);
         sectionAnimes.appendChild(listAnimes);
-        listAnimes.addEventListener('click', handleClickFav)
+        listAnimes.addEventListener('click', handleClickFav);
     
         const title = document.createElement('h2');
         const titleName = document.createTextNode(eachAnime.title);
@@ -40,19 +40,19 @@ const renderAnimes = (animes)=>{
         listAnimes.appendChild(title);
 
         const image = document.createElement('img');
-        image.setAttribute('src', eachAnime.images.jpg.image_url)
+        image.setAttribute('src', eachAnime.images.jpg.image_url);
         image.setAttribute('alt', eachAnime.title);
         listAnimes.appendChild(image);
 
-        const findAnimeFavorite = animesFavorites.find((item)=>item.mal_id === eachAnime.mal_id); //si el objeto con id es igual al id del clicado, añade la clase favorite
+        const findAnimeFavorite = animesFavorites.find((item)=>item.mal_id === eachAnime.mal_id);
         if (findAnimeFavorite){
             listAnimes.setAttribute('class', 'favorite');
         }
     }
 }
-//renderizar la nueva función render con los favoritos
+
 const renderAnimesFavorites = (animesFavorites)=>{
-    sectionAnimesFavorites.innerHTML = ''
+    sectionAnimesFavorites.innerHTML = '';
     const titleSectionFavorites = document.createElement('h3');
     const textTitleSectionFavorites = document.createTextNode('Favoritos');
     titleSectionFavorites.appendChild(textTitleSectionFavorites);
@@ -68,7 +68,7 @@ const renderAnimesFavorites = (animesFavorites)=>{
         listAnimesFavorite.appendChild(titleFavorite);
 
         const imageFavorite = document.createElement('img');
-        imageFavorite.setAttribute('src', eachAnimeFavorite.images.jpg.image_url)
+        imageFavorite.setAttribute('src', eachAnimeFavorite.images.jpg.image_url);
         imageFavorite.setAttribute('alt', eachAnimeFavorite.title);
         listAnimesFavorite.appendChild(imageFavorite);
     }
@@ -78,24 +78,25 @@ const getDataApi = (value)=>{
     fetch (`https://api.jikan.moe/v4/anime?q=${value}`)
     .then ((response)=> response.json())
     .then(data=>{
-        // console.log(data);
-        arrayAnime = data.data; //objeto.array (datos del servidor)
-        renderAnimes(arrayAnime); //cuando llamo a la función renderAnimes, le estoy poniendo como parámetro el arrayAnime = a los datos del servidor
-});
+        arrayAnime = data.data;
+        renderAnimes(arrayAnime);
+    });
 }
+
 const handleClickSearch = (event)=>{
     event.preventDefault();
     const valueInput = input.value;
-    getDataApi(valueInput); //le pongo como parámetro a la función getDataApi el valor del input
+    getDataApi(valueInput);
 }
 searchBtn.addEventListener('click', handleClickSearch);
 
-getDataApi('naruto'); //una vez que tengo los datos, los pinto al iniciar la página
-// aquí tendría que renderizar los animesFavorites al cargar la página
+getDataApi('naruto');
+
 const getAnimeFavoriteDataSave = JSON.parse(localStorage.getItem('animesFavorites'));
+
 if(getAnimeFavoriteDataSave){
-    animesFavorites = getAnimeFavoriteDataSave; //segundo: el array de favoritos primero se pasa como vacío en la línea siete e inmediatamente después se rellena con los datos del localstorage
-    renderAnimesFavorites(animesFavorites); //volvemos a renderizar el array
+    animesFavorites = getAnimeFavoriteDataSave;
+    renderAnimesFavorites(animesFavorites);
 }
 
 
